0ffcebeee33c4dbe55b44d3f60898cf44380f18b
Shivani Parhad <shivani11parhad@gmail.com>
2026-04-12 15:03:04 +0530

    created the FeedHtml function to display the feed from provided data
    created a render function to render add the feed to DOM

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 import {tweetsData} from './data.js'
-console.log(tweetsData)
+
+
+const tweetBtn = document.getElementById('tweet-btn')
+const tweetInput = document.getElementById('tweet-input')
+
+// testing the text input
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+  
+})
+
+function getFeedHtml () {
+    let feedHTML = ''
+    tweetsData.forEach(function (tweet){
+    feedHTML += `<div class="tweet">
+		<div class="tweet-inner">
+			<img src="${tweet.profilePic}" class="profile-pic">
+			<div>
+				<p class="handle">${tweet.handle}</p>
+				<p class="tweet-text">${tweet.tweetText}</p>
+				<div class="tweet-details">
+					<span class="tweet-detail">
+						${tweet.replies.length}
+					</span>
+					<span class="tweet-detail">
+						${tweet.likes}
+					</span>
+					<span class="tweet-detail">
+						${tweet.retweets}
+					</span>
+				</div>
+			</div>
+	</div>
+</div>`
+    
+})
+return feedHTML
+}
+
+function render () {
+    document.getElementById('feed').innerHTML = getFeedHtml()
+
+}
+
+render()
+
+
+
+
+// adding the boilerplate
