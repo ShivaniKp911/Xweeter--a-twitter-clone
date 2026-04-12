a9ddf37d876de9856e55044169e7492b841d70e9
Shivani Parhad <shivani11parhad@gmail.com>
2026-04-12 17:10:15 +0530

    Added like reply and retweet buttons  from font awesome cdn

--- index.js
+++ index.js
@@ -20,13 +20,17 @@ function getFeedHtml () {
 				<p class="handle">${tweet.handle}</p>
 				<p class="tweet-text">${tweet.tweetText}</p>
 				<div class="tweet-details">
+                
 					<span class="tweet-detail">
+                     <i class="fa-regular fa-comment-dots"></i>
 						${tweet.replies.length}
 					</span>
 					<span class="tweet-detail">
+                     <i class="fa-regular fa-heart"></i>
 						${tweet.likes}
 					</span>
 					<span class="tweet-detail">
+                     <i class="fa-solid fa-retweet"></i>
 						${tweet.retweets}
 					</span>
 				</div>
